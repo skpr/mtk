ec0c85fd3d82c3e7bef76d204588f3a4e89a2211
Nick Schuch <nick@myschuch.com>
2020-03-24 14:44:59 +1000

    Fix environment variables

--- dump/pkg/envar/envar.go
+++ dump/pkg/envar/envar.go
@@ -2,19 +2,19 @@ package envar
 
 const (
 	// MaxConn defines the environment variable when using the command line.
-	MaxConn = "MTK_DUMP_MAX_CONN"
+	MaxConn = "MYSQL_MAX_CONN"
 	// Config defines the environment variable when using the command line.
-	Config = "MTK_DUMP_CONFIG"
+	Config = "MYSQL_CONFIG"
 	// Hostname defines the environment variable when using the command line.
-	Hostname = "MTK_DUMP_HOSTNAME"
+	Hostname = "MYSQL_HOSTNAME"
 	// Username defines the environment variable when using the command line.
-	Username = "MTK_DUMP_USERNAME"
+	Username = "MYSQL_USERNAME"
 	// Password defines the environment variable when using the command line.
-	Password = "MTK_DUMP_PASSWORD"
+	Password = "MYSQL_PASSWORD"
 	// Protocol defines the environment variable when using the command line.
-	Protocol = "MTK_DUMP_PROTOCOL"
+	Protocol = "MYSQL_PROTOCOL"
 	// Port defines the environment variable when using the command line.
-	Port = "MTK_DUMP_PORT"
+	Port = "MYSQL_PORT"
 	// Database defines the environment variable when using the command line.
-	Database = "MTK_DUMP_DATABASE"
+	Database = "MYSQL_DATABASE"
 )
