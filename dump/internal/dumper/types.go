4e83f87cb7f8c2c37e716ffe21f1fce2cdedba95
Nick Schuch <nick@myschuch.com>
2023-04-17 14:50:41 +1000

    Adds comments

--- dump/internal/dumper/types.go
+++ dump/internal/dumper/types.go
@@ -1,12 +1,16 @@
 package dumper
 
+// TableType used to identify what type the table is.
 type TableType string
 
 const (
+	// TypeTableBase are the underlying tables that actually store the metadata for a specific database.
 	TypeTableBase TableType = "BASE TABLE"
+	// TypeTableView is a virtual table based on the result-set of an SQL statement.
 	TypeTableView TableType = "VIEW"
 )
 
+// Table is a data set consisting of columns and rows.
 type Table struct {
 	Name string
 	Type TableType
