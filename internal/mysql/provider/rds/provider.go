1c1dd03786fec79b81f12ed842f01f1745686314
Parth Gohil <138430690+parthpnx@users.noreply.github.com>
2025-03-17 13:53:10 +0300

    fix: set character encoding when importing file from csv in s3

--- internal/mysql/provider/rds/provider.go
+++ internal/mysql/provider/rds/provider.go
@@ -68,7 +68,7 @@ func (d *Client) GetLoadQueryForTable(table string) (string, error) {
 	}
 	path := strings.TrimPrefix(d.URI, "s3://")
 	query := fmt.Sprintf("LOAD DATA FROM S3 MANIFEST 'S3-%s://%s/%s.csv.manifest' INTO TABLE `%s`", d.Region, path, table, table)
-	query = fmt.Sprintf("%s FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n'", query)
+	query = fmt.Sprintf("%s FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n' CHARACTER SET utf8mb4", query)
 
 	return query, nil
 }
