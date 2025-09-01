ee80ce76f8553deb1ee64698a9ab3ed9e92f5a89
Nick Schuch <nick@previousnext.com.au>
2025-09-01 16:22:14 +1000

    RDS provider uses writer for dump output

--- internal/mysql/provider/stdout/provider.go
+++ internal/mysql/provider/stdout/provider.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"io"
 	"log"
 	"strings"
 
@@ -26,11 +27,11 @@ func NewClient(conn *sql.Conn, logger *log.Logger) *Client {
 	}
 }
 
-// GetSelectQueryForTable will return a complete SELECT query to fetch data from a table.
-func (d *Client) GetSelectQueryForTable(ctx context.Context, table string, params provider.DumpParams) (string, error) {
+// WriteTableData will write the data from a table to the provided writer.
+func (d *Client) WriteTableData(ctx context.Context, w io.Writer, table string, params provider.DumpParams) error {
 	cols, err := providerutils.QueryColumnsForTable(ctx, d.Conn, table, params)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	query := fmt.Sprintf("SELECT %s FROM `%s`", strings.Join(cols, ", "), table)
@@ -39,5 +40,72 @@ func (d *Client) GetSelectQueryForTable(ctx context.Context, table string, param
 		query = fmt.Sprintf("%s WHERE %s", query, where)
 	}
 
-	return query, nil
+	rows, err := d.Conn.QueryContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	values := make([]*sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	var (
+		counter  = 0
+		firstRun = true
+	)
+
+	for rows.Next() {
+		// We have already done a loop and need to close the previous insert statement.
+		if counter >= params.ExtendedInsertRows {
+			fmt.Fprintln(w, ";")
+			counter = 0
+		} else {
+			if !firstRun {
+				fmt.Fprint(w, ",")
+			}
+		}
+
+		if counter == 0 {
+			fmt.Fprintf(w, "INSERT INTO `%s` VALUES ", table)
+		}
+
+		counter++
+
+		firstRun = false
+
+		if err = rows.Scan(scanArgs...); err != nil {
+			return err
+		}
+
+		var vals []string
+
+		for _, col := range values {
+			val := "NULL"
+
+			if col != nil {
+				val, err = getValue(string(*col))
+				if err != nil {
+					return err
+				}
+			}
+
+			vals = append(vals, val)
+		}
+
+		fmt.Fprintf(w, "(%s)", strings.Join(vals, ","))
+	}
+
+	fmt.Fprintln(w, ";")
+
+	return nil
 }
